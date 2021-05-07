cc09346cdfe2d683aa27f41f1c6ecbb7d28f20be
SagarKevin <75674393+SagarKevin@users.noreply.github.com>
2021-05-07 12:10:41 +0100

    Update maps.js
    Updates based on first feedback.

--- time-dimension-example/maps.js
+++ time-dimension-example/maps.js
@@ -3,19 +3,22 @@ var erddapURL = 'https://erddap.marine.ie/erddap' ;
 var datasetID = 'compass_neatl_hindcast_grid' ; 
 var erddap = new ERDDAP(erddapURL);
 var ds = erddap.dataset(datasetID);
+var timeRange;
 
 var timeForm = document.getElementById('form');
 var startTimeInput = document.getElementById('from');
 var endTimeInput = document.getElementById('to');
 
+var startDate = new Date(startTimeInput.value);
+var endDate = new Date(endTimeInput.value);
+
 var marker = null;
 
 function updateTimeRange() {
-	var startDate = new Date(startTimeInput.value);
-	var endDate = new Date(endTimeInput.value);	
-	var times = mymap.timeDimension.options.times;
+	startDate = new Date(startTimeInput.value);
+	endDate = new Date(endTimeInput.value);	
 	
-	timeInterval = times.filter(a => a > startDate && a < endDate);
+	timeInterval = timeRange.filter(a => a > startDate && a < endDate);
 	mymap.timeDimension.setAvailableTimes(timeInterval, 'replace');
 	mymap.timeDimension.setCurrentTime(startDate);
 
@@ -28,9 +31,9 @@ function updateTimeRange() {
 
 ds.fetchTimeDimension().then(function(times){
 	
-	times = times.map(function(d){return new Date(d)});
+	timeRange = times.map(function(d){return new Date(d)});
+	timeInterval = timeRange.filter(a => a > startDate && a < endDate);
 
-	
 	mymap = L.map('map', {
 		zoom: 6,
 		center: [56.5, -7],
@@ -38,7 +41,7 @@ ds.fetchTimeDimension().then(function(times){
 		timeDimensionControl: true,
 		timeDimension: true,
 		timeDimensionOptions: {
-		   times: times
+		   times: timeInterval
 		},
 		timeDimensionControlOptions: {
 		  maxSpeed: 2,
@@ -125,65 +128,69 @@ ds.fetchTimeDimension().then(function(times){
 		}
 	});
 	
-	var wmsUrl = "https://data.marine.ie/ncWMS/wms" ;
-	
-	var surfTemp = L.tileLayer.wms(wmsUrl, {
+	var wmsUrl = "https://data.marine.ie/ncWMS/wms?SERVICE=WMS&REQUEST=GetCapabilities&VERSION=1.1.1&STYLES=default-scalar/default&version=1.1.1&PALETTE=default" ;
+	var tempColourRange = "0,23"
+	var salColourRange = "0,40"
+	var tempWmsURL = wmsUrl+"&COLORSCALERANGE="+tempColourRange;
+	var salWmsURL = wmsUrl+"&COLORSCALERANGE="+salColourRange;
+
+	var surfTemp = L.tileLayer.wms(tempWmsURL, {
 		layers: 'COMPASS_NEATL/sea_surface_temperature',
 		format: 'image/png',
 		transparent: true});
 		
 	var surfTempTime = L.timeDimension.layer.wms(surfTemp);
 		
-	var surfSal = L.tileLayer.wms(wmsUrl, {
+	var surfSal = L.tileLayer.wms(salWmsURL, {
 		layers: 'COMPASS_NEATL/sea_surface_salinity',
 		format: 'image/png',
 		transparent: true});
 		
 	var surfSalTime = L.timeDimension.layer.wms(surfSal);
 		
-	var tenTemp = L.tileLayer.wms(wmsUrl, {
+	var tenTemp = L.tileLayer.wms(tempWmsURL, {
 		layers: 'COMPASS_NEATL/sea_temperature_at_10_meters_depth',
 		format: 'image/png',
 		transparent: true});
 		
 	var tenTempTime = L.timeDimension.layer.wms(tenTemp);
 		
-	var tenSal = L.tileLayer.wms(wmsUrl, {
+	var tenSal = L.tileLayer.wms(salWmsURL, {
 		layers: 'COMPASS_NEATL/sea_salinity_at_10_meters_depth',
 		format: 'image/png',
 		transparent: true});
 		
 	var tenSalTime = L.timeDimension.layer.wms(tenSal);
 
-	var thirtyTemp = L.tileLayer.wms(wmsUrl, {
+	var thirtyTemp = L.tileLayer.wms(tempWmsURL, {
 		layers: 'COMPASS_NEATL/sea_temperature_at_30_meters_depth',
 		format: 'image/png',
 		transparent: true});
 		
 	var thirtyTempTime = L.timeDimension.layer.wms(thirtyTemp);
 
-	var thirtySal = L.tileLayer.wms(wmsUrl, {
+	var thirtySal = L.tileLayer.wms(salWmsURL, {
 		layers: 'COMPASS_NEATL/sea_salinity_at_30_meters_depth',
 		format: 'image/png',
 		transparent: true});
 		
 	var thirtySalTime = L.timeDimension.layer.wms(thirtySal)
 
-	var bottomTemp = L.tileLayer.wms(wmsUrl, {
+	var bottomTemp = L.tileLayer.wms(tempWmsURL, {
 		layers: 'COMPASS_NEATL/sea_bottom_temperature',
 		format: 'image/png',
 		transparent: true});
 		
 	var bottomTempTime =  L.timeDimension.layer.wms(bottomTemp);
 		
-	var bottomSal = L.tileLayer.wms(wmsUrl, {
+	var bottomSal = L.tileLayer.wms(salWmsURL, {
 		layers: 'COMPASS_NEATL/sea_bottom_salinity',
 		format: 'image/png',
 		transparent: true});
 		
 	var bottomSalTime =  L.timeDimension.layer.wms(bottomSal);
 		
-	var mixedDepth = L.tileLayer.wms(wmsUrl, {
+	var mixedDepth = L.tileLayer.wms(wmsUrl+"&COLORSCALERANGE=10,400", {
 		layers: 'COMPASS_NEATL/mixed_layer_depth',
 		format: 'image/png',
 		transparent: true});
@@ -218,6 +225,32 @@ ds.fetchTimeDimension().then(function(times){
 
 	var layerControl = L.control.groupedLayers(baseLayers,groupedOverlays);
 	layerControl.addTo(mymap);
+	
+	var layerLegend = document.getElementById('legend');
+
+	mymap.on('overlayadd', function (e) {
+		var activeLayer = e.layer;
+		var activeLayerName = activeLayer._currentLayer.options.layers;
+		var legendUrl = "http://data.marine.ie/ncWMS/wms?REQUEST=GetLegendGraphic&PALETTE=default&LAYERS="
+						+activeLayerName
+						+"&STYLES=default-scalar/default"
+		if (activeLayerName.includes("temp")){
+			layerLegend.src = legendUrl+"&COLORSCALERANGE="+tempColourRange;
+			layerLegend.style.display = "block"
+		}
+		else if (activeLayerName.includes("sal")){
+			layerLegend.src = legendUrl+"&COLORSCALERANGE="+salColourRange
+			layerLegend.style.display = "block"
+		}
+		else if (activeLayerName.includes("mixed")){
+			layerLegend.src = legendUrl+"&COLORSCALERANGE=10,400";
+			layerLegend.style.display = "block";
+		}	
+	});
+
+	mymap.on('overlayremove', function (e) {
+		layerLegend.style.display = "none"
+	});
 
 	var popup = L.popup();
 
